425f5887b6179e001d35fc6740ccfa45e559e387
Luca Guariento <lguariento@users.noreply.github.com>
2025-03-13 14:48:57 +0000

    Tagulous settings
    Settings for Tagulous added.

--- config/settings.py
+++ config/settings.py
@@ -197,6 +197,13 @@ LOGGING = {
     },
 }
 
+SERIALIZATION_MODULES = {
+    'xml':    'tagulous.serializers.xml_serializer',
+    'json':   'tagulous.serializers.json',
+    'python': 'tagulous.serializers.python',
+    'yaml':   'tagulous.serializers.pyyaml',
+}
+
 # -------------- Haystack settings --------------
 HAYSTACK_CONNECTIONS = {
     "default": {
