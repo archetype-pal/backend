c830462cd1c68f968b0f88c1ab45523799d86da7
Phaedon <faidon.koutsogiannopoulos@glasgow.ac.uk>
2025-05-15 20:58:44 +0100

    refactor: add more environment variables based on the issue #35

--- apps/common/models.py
+++ apps/common/models.py
@@ -1,12 +1,18 @@
 from django.db import models
-
+from django.conf import settings
 
 class Date(models.Model):
     date = models.CharField(max_length=100)
     # Use the following two fields to represent the date as a numeric value
     #   This way, it can be used for sorting.
-    min_weight = models.IntegerField("Minimum weight", help_text="The lower bound of the date range")
-    max_weight = models.IntegerField("Maximum weight", help_text="The upper bound of the date range")
+    min_weight = models.IntegerField(
+        verbose_name=settings.FIELD_DISPLAY_NAME_DATE_MIN, 
+        help_text="The lower bound of the date range"
+    )
+    max_weight = models.IntegerField(
+        verbose_name=settings.FIELD_DISPLAY_NAME_DATE_MAX,
+        help_text="The upper bound of the date range"
+    )
 
     def __str__(self):
         return self.date
@@ -16,3 +22,6 @@ class Date(models.Model):
             "min_weight": self.min_weight,
             "max_weight": self.max_weight,
         }
+
+    class Meta:
+        verbose_name = settings.MODEL_DISPLAY_NAME_DATE
