e02b9fe4f453fc7abfde7bf0c9929c427a325e49
Ahmed Elghareeb <me@rancho.me>
2026-01-27 17:59:48 +0100

    chore: local dev compose tweaks and Celery config style
    - compose: optional ES port comments, sipi volume path
    - settings: use double quotes for Celery config

--- config/settings.py
+++ config/settings.py
@@ -192,11 +192,11 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 # Celery Configuration
-CELERY_BROKER_URL = 'redis://redis:6379/0'
-CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
+CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_RESULT_BACKEND = "redis://redis:6379/0"
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
