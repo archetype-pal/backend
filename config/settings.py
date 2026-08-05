118df4e0b6d1a85557f54507d01c9cb3ce7f9e69
Anthony Geourjon <anthony.geourjon@tetras-libre.fr>
2026-08-05 13:55:44 +0200

    Use config/.env in Django settings.py

--- config/settings.py
+++ config/settings.py
@@ -6,9 +6,9 @@ import dj_database_url
 from django.core.exceptions import ImproperlyConfigured
 import environ
 
-# Load .env from project root when running outside Docker (e.g. manage.py runserver, pytest)
+# Load .env from config/ when running outside Docker (e.g. manage.py runserver, pytest)
 BASE_DIR = Path(__file__).resolve().parent.parent
-_env_file = BASE_DIR / ".env"
+_env_file = BASE_DIR / "config" / ".env"
 environ.Env.read_env(_env_file)
 
 env = environ.Env(
